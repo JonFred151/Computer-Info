b83f5e045513f6246a69632ffe67849577c5e1a9
Jon Frederick <jonFred151@gmail.com>
2025-02-01 12:08:04 -0600

    first push

---
+++ Computer-info/Program.cs
@@ -0,0 +1,45 @@
+﻿//Begining of program
+//this is a test line
+using System;
+using System.Net.NetworkInformation;
+namespace Computer
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+                //int IpAdress;
+                var name = System.Environment.MachineName;
+                Console.WriteLine(name);
+               Console.WriteLine( PingHost(name));
+
+            }
+
+        public static bool PingHost(string name)
+            {
+            try
+                {
+                    using (Ping pinger = new Ping())
+                    {
+                    PingReply reply = pinger.Send(name);
+                    return reply.Status == IPStatus.Success;   
+                    }
+                }
+            catch (PingException)
+            {
+                return false;
+            }
+            }
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
