a58ed8982e57060d5e28e0d225a8e38ba500705b
Jon Frederick <jonFred151@gmail.com>
2025-02-02 11:55:59 -0600

    Add functionally to get Ip and started the process for get program list

--- Computer-info/Program.cs
+++ Computer-info/Program.cs
@@ -1,19 +1,58 @@
 ﻿//Begining of program
 //this is a test line
 using System;
+using System.Text;
+using System.Net;
 using System.Net.NetworkInformation;
+using Microsoft.Win32;
 namespace Computer
     {
     class Program
         {
         static void Main(string[] args)
             {
-                //int IpAdress;
+                
                 var name = System.Environment.MachineName;
                 Console.WriteLine(name);
                Console.WriteLine( PingHost(name));
 
+            string IpAddress = Dns.GetHostName();
+            Console.WriteLine(IpAddress);
+
+            string IP =Dns.GetHostEntry(IpAddress).AddressList[4].ToString();
+            Console.WriteLine("IP Address is : " + IP);
+            FindByDisplayName();
+            
+        }
+
+        
+
+        private static string FindByDisplayName(RegistryKey parentKey, string name)
+        {
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.GetValue("DisplayName").ToString() == name)
+                    {
+                        return regKey.GetValue("installLocation").ToString();
+                    }
+                }
+                catch { }
             }
+            return " ";
+        }
+
+        //public static string HostIP()
+        //{
+        //    var host = Dns.GetHostEntry(Dns.GetHostName());
+        //    foreach (var ip in host.AddressList)
+        //    {
+        //        if(ip.AddressFamily == AddressFamily.InterNetwork)
+        //    }
+        //}
 
         public static bool PingHost(string name)
             {
--- Computer-info/obj/Debug/net8.0/Computer-info.AssemblyInfo.cs
+++ Computer-info/obj/Debug/net8.0/Computer-info.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Computer-info")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74a3d06f83059b01d84cbc75849384f563c98b7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b83f5e045513f6246a69632ffe67849577c5e1a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Computer-info")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Computer-info")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
